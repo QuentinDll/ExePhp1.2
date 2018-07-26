640ca42267c432d23a8edb7b675247927dceb484
QuentinDll <quentin.delplace13@gmail.com>
2018-07-26 16:24:26 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 1</title>
+</head>
+<body>
+  <p>
+    <?php
+    $age = 21;
+
+    if ($age < 18){
+      echo "Vous êtes mineur";
+    }else{
+      echo "Vous êtes majeur";
+    }
+    ?>
+  </p>
+</body>
+</html>
